c91a3356ab6daa5b58547b8c77d9d26499c74baa
Victor González <victorglzgmz12@gmail.com>
2023-10-30 14:11:03 -0600

    Conexion con NodeJS
    conexion de prueba establecido entre python y nodejs

--- proyectoUbicuo/main.py
+++ proyectoUbicuo/main.py
@@ -1,10 +1,13 @@
 import mysql.connector
 import requests
-conn = mysql.connector.connect(host='sql3.freesqldatabase.com', password='ZzRpEBZpe8', user='sql3657983', database='sql3657983')
-'''url = 'http://localhost:3000/auth'
-r = requests.get(url)
-data = r.json()'''
 
+conn = mysql.connector.connect(host='sql3.freesqldatabase.com', password='ZzRpEBZpe8', user='sql3657983', database='sql3657983')
+
+url = 'http://localhost:3000/auth'
+r = requests.get(url)
+print(r.text)
+
+#Confirmar la conexion con mysql
 if conn.is_connected():
     print("Connection established..")
 
@@ -12,10 +15,12 @@ mycursor = conn.cursor()
 
 mycursor.execute("SELECT * FROM datosArboles")
 myresult = mycursor.fetchall()
-print(type(myresult))
+#print(type(myresult))
 
 for x in myresult:
   print(x)
 
+#Guardar el resultado en un TXT
 with open("file.txt", "w") as output:
     output.write(str(myresult))
+
