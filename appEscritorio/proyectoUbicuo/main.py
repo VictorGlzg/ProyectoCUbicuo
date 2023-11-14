1dba040cff654ed838b5822f2b10e86eb0b2eaf8
Victor González <victorglzgmz12@gmail.com>
2023-11-14 00:32:25 -0600

    app ciclo infinito

--- appEscritorio/proyectoUbicuo/main.py
+++ appEscritorio/proyectoUbicuo/main.py
@@ -7,7 +7,7 @@ conn = mysql.connector.connect(host='sql3.freesqldatabase.com', password='ZzRpEB
 mycursor = conn.cursor()
 
 # PETICION POST AL SERVIDOR
-url = 'https://chubby-crabs-march.loca.lt/escritorio'
+url = 'https://spicy-views-repair.loca.lt/escritorio'
 
 #Confirmar la conexion con mysql
 if conn.is_connected():
@@ -30,22 +30,6 @@ def update():
     # Devolver este diccionario
     return respuesta
 
-#Guardar el resultado en un TXT
-'''with open("file.txt", "w") as output:
-    output.write(str(myresult))'''
-#Obtener el ultimo registro no clasificados para clasificado.
-#ORDER BY id_registro DESC LIMIT 1
-registrosArboles = consulta("SELECT * FROM registrosArboles WHERE buenaCondicion IS NULL ORDER BY id_registro DESC LIMIT 1")
-#id_registro, id_arbol,humedad,temperaturaAmb,humedadAmb,fecha,hora,buenaCondicion
-# 0             1           2         3          4        5     6        7
-
-idTipoArbol = 1;
-# registrosArboles[0][0] obtener el id del arbol
-datosArboles = consulta("SELECT * FROM datosArboles WHERE id_arbol ="+str(idTipoArbol))
-#id_arbol, nombre,nombreCientifico,humMinima,humMaxima,tempAmbientalMinima,humAmbientalMinima
-# 0          1          2               3        4              5                   6
-humMinima = datosArboles[0][3]
-humMaxima = datosArboles[0][4]
 
 def clasificar():
     id = arbol[0]
@@ -61,38 +45,40 @@ def clasificar():
         estado = "Buen estado"
         condicion = 1
     # Establecer condicion en BD
-    mycursor.execute("UPDATE registrosArboles SET buenaCondicion=" + str(condicion) + " WHERE id_registro = " + str(id))
+    mycursor.execute(
+        "UPDATE registrosArboles SET buenaCondicion=" + str(condicion) + " WHERE id_registro = " + str(id))
     conn.commit()
     print(mycursor.rowcount, "record(s) affected")
 
-    myobj = {'noti': 'Los datos censados actuales son: humedad:'+str(humedad)+'temperatura ambiental:'+str(temperaturaAmb)
-             +'humedad ambiental:'+str(humedadAmb)+' el arbol se encuentra en: '+estado}
+    myobj = {'noti': 'Los datos censados actuales son: humedad:' + str(humedad) + 'temperatura ambiental:' + str(
+        temperaturaAmb)
+                     + 'humedad ambiental:' + str(humedadAmb) + ' el arbol se encuentra en: ' + estado}
     requests.post(url, json=myobj)
     print(requests.get(url))
 
-#CLASIFICACION DE ACUERDO A HUMBRAL DE HUMEDAD
-for arbol in registrosArboles:
-    clasificar()
+#Guardar el resultado en un TXT
+'''with open("file.txt", "w") as output:
+    output.write(str(myresult))'''
 
+while(True):
+    r = requests.get(url)
+    if(r != None):
+        print("Consulta")
+        # Obtener el ultimo registro no clasificados para clasificado.
+        # ORDER BY id_registro DESC LIMIT 1
+        registrosArboles = consulta("SELECT * FROM registrosArboles WHERE buenaCondicion IS NULL ORDER BY id_registro DESC LIMIT 1")
+        # id_registro, id_arbol,humedad,temperaturaAmb,humedadAmb,fecha,hora,buenaCondicion
+        # 0             1           2         3          4        5     6        7
 
-#Recibir peticiones del servidor
-'''
-import socket
+        idTipoArbol = 1;
+        # registrosArboles[0][0] obtener el id del arbol
+        datosArboles = consulta("SELECT * FROM datosArboles WHERE id_arbol ="+str(idTipoArbol))
+        # id_arbol, nombre,nombreCientifico,humMinima,humMaxima,tempAmbientalMinima,humAmbientalMinima
+        # 0          1          2               3        4              5                   6
+        humMinima = datosArboles[0][3]
+        humMaxima = datosArboles[0][4]
 
-host = '127.0.0.1'
-port = 80
-buffer_size = 20  
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen(1)
-
-conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(buffer_size)
-if not data: break
-print("received data:", data)
-conn.send(data)  # echo
-conn.close()
-'''
+        # CLASIFICACION DE ACUERDO A HUMBRAL DE HUMEDAD
+        for arbol in registrosArboles:
+            clasificar()
+        r = None
